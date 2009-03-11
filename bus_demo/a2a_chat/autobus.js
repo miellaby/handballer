ad6aecb1a8705111117c5b0945d04882c4a73150
sylvain.garden <sylvain.garden@5f132cf0-ffa0-11dd-a3ef-1f8437fb274a>
2009-03-11 07:42:19 +0000

    a2a_chat work in progress

--- bus_demo/a2a_chat/autobus.js
+++ bus_demo/a2a_chat/autobus.js
@@ -2,20 +2,57 @@
 // autobus
 // =========================================================================
 
-var indexAgent = new busAgent();
+var indexAgent = new PubSubAgent();
 
-indexAgent.index = function(name, object) {
+indexAgent.cbPushList = {};
+indexAgent.cbRemoveList = {};
+
+indexAgent.push = function(name, object) {
   if (this[name] === undefined) this[name] = [];
   this[name].push(object);
+
+  if (this.cbPushList[variable] !== undefined)
+    for (var lst = this.cbPushList[variable], l = lst.length, i = l - 1; i >= 0; i--)
+      lst[i](variable, object);
 }
 
-indexAgent.getIndex = function(name) {
+indexAgent.remove = function(name, object) {
+  if (this[name] === undefined) return;
+  this[name].remove(object);
+
+  if (this.cbRemoveList[variable] !== undefined)
+    for (var lst = this.cbRemoveList[variable], l = lst.length, i = l - 1; i >= 0; i--)
+      lst[i](variable, object);
+}
+
+indexAgent.getIndex = indexAgent.getfunction(name) {
   if (this[name] === undefined) this[name] = [];
   return this[name];
 }
 
 indexAgent.isIndexed = function(name, object) {
   return (this[name] && this[name].indexOf(object) != -1);
+}
+
+indexAgent.setSubscribe = function (variable, cbPush, cbRemove) {
+  if (cbPush) {
+     if (this.cbPushList[variable])
+        this.cbPushList[variable].push(cbPush);
+     else
+       this.cbPushList[variable] = [cbPush];
+  }
+
+  if (cbRemove) {
+     if (this.cbRemoveList[variable])
+        this.cbRemoveList[variable].push(cbRemove);
+     else
+        this.cbRemoveList[variable] = [cbRemove];
+  }
+}
+
+indexAgent.setUnsubscribe = function (variable, cbPush, cbRemove) {
+  if (cbPush) this.cbPushList[variable].remove(cbPush);
+  if (cbRemove) this.cbRemoveList[variable].remove(cbRemove);
 }
 
 // ======
@@ -45,8 +82,10 @@ function autobusMessageCB(label, body) {
       }
       if (command == "set") {
         obj.set(parameter, body);
-      } else {
-        obj[command].call([parameter, body], obj);
+      } else if (command == "category") {
+        parameter.splice("/").forEach(function(c) { obj.
+      } {
+        obj.call(command, body);
       }
     } else if (label.substring(0,6) == "model/") {
 
