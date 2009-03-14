2dae1e2120bbbf5e0a720258834e9d86035ef05a
sylvain.garden <sylvain.garden@5f132cf0-ffa0-11dd-a3ef-1f8437fb274a>
2009-03-14 08:00:05 +0000

    a2a chat

--- bus_demo/a2a_chat/busAgent.js
+++ bus_demo/a2a_chat/busAgent.js
@@ -1,20 +1,24 @@
 // library "Bus Agent" :
 // A Bus Agent is a Pub/Sub Agent distributed over the web thanks to Handballer HTTP Bus.
-// If it is "here", you may directly set its attributs
-// It it is "there", you may set a value by pass it through the bus
+// - If it is "here", you may directly set its attributs
+// - It it is "there", you may set a value by pass it through the bus
+// - It also may be "both" here and there!
 // =========================================================================
 
 function BusAgent(autobus, name, here) {
   PubSubAgent.call(this);
   this.autobus = autobus;
   this.name = name;
-  this.here = (here === undefined ? false : here);
+  this.location = (here === undefined ? BusAgent.prototype.there : here);
   this.autobus.tagsonomy.push("agents", this);
   this.autobus.tagsonomy.push(name, this);
 };
 
 BusAgent.prototype = new PubSubAgent();
 BusAgent.prototype.constructor = BusAgent;
+BusAgent.prototype.here = 1;
+BusAgent.prototype.there = 2;
+BusAgent.prototype.both = 3;
 
 BusAgent.prototype.setted = function(variable, value) {
   var oldValue = this[variable]; // to trace change in particular cases
@@ -23,12 +27,13 @@ BusAgent.prototype.setted = function(variable, value) {
 
   value = this[variable]; // it may have change
 
-  if (value != oldValue) {
-    if (variable == "name") {
+  if (value == oldValue) return;
+
+  if (variable == "name") {
        if (oldValue) this.autobus.tagsonomy.remove(oldValue, this);
        this.autobus.tagsonomy.add(oldValue, this);
 
-    } else if (variable == "tags") {
+  } else if (variable == "tags") {
        var i, l;
        if (!oldValue) oldValue = [];
        for (i = 0, l = oldValue.length; i < l; i++) {
@@ -41,16 +46,15 @@ BusAgent.prototype.setted = function(variable, value) {
           if (oldValue.indexOf(v) == -1)
              this.autobus.tagsonomy.push(v, this);
        }
-    }
   }
 
   var name = (variable == "name" ? oldValue : this.name);
-  if (this.here && name)
+  if (this.location !== BusAgent.prototype.there && name)
      this.autobus.hbc.send("model/" + name + "/" + variable, jsonize(value));
 }
 
 BusAgent.prototype.set = function(variable, value) {
-   if (this.here) {
+   if (this.location !== BusAgent.prototype.there) {
        this.setted(variable, value);
    } else {
        this.autobus.hbc.send("control/" + this.name + "/set/" + variable, jsonize(value));
@@ -58,7 +62,7 @@ BusAgent.prototype.set = function(variable, value) {
 }
 
 BusAgent.prototype.call = function(fnName) {
-   if (this.here) {
+   if (this.location !== BusAgent.prototype.there) {
        this[fnName].apply(this, arguments.splice(1));
    } else {
        this.autobus.hbc.send("control/" + this.name + "/" + fnName, jsonize(arguments.splice(1)));
