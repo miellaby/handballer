661450a58890416d247bd4c9fd0715f66116ee20
sylvain.garden <sylvain.garden@5f132cf0-ffa0-11dd-a3ef-1f8437fb274a>
2009-06-03 00:17:36 +0000

    pubSub framework refactored : no more indexAgent

--- trunk/bus_demo/a2a_chat/indexAgent.js
+++
@@ -1,5 +0,0 @@
-// =========================================================================
-// indexAgent: a pub/sub agent "class" managing subscribable indexes of objects
-// =========================================================================
-
-// removed: pubSubAgent can make it
