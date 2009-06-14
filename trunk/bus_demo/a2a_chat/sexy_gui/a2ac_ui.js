4d49e7de590bac31fa5927543dc570e39f13ab65
sylvain.garden <sylvain.garden@5f132cf0-ffa0-11dd-a3ef-1f8437fb274a>
2009-06-14 00:38:06 +0000

    things go

--- trunk/bus_demo/a2a_chat/sexy_gui/a2ac_ui.js
+++ trunk/bus_demo/a2a_chat/sexy_gui/a2ac_ui.js
@@ -1,8 +1,45 @@
+var contextForm  = {};
+function showContextForm(e) {
+    if (contextForm.element)
+        contextForm.element.style.display = "none";
+    contextForm.target = e;
+    if (!e) return;
+    var isMe = (e.item === me);
+    var c = contextForm.element = isMe? document.getElementById("contextForm") : null;
+    if (!c) return;
+    c.style.display = "block";
+    var coords = readDOM(e.img);
+    var coords2 = {
+        x: coords.x + coords.w * 0.33,
+        y: coords.y + coords.h * 0.66
+    };
+    writeDOM(c, coords2); 
+}
+
+function submitContextForm(e) {
+    me.set('nickname', document.getElementById('meName').value);
+    me.set('emblem', document.getElementById('meEmblem').value);
+    me.set('mind', document.getElementById('meMind').value);
+    me.set('icon', document.getElementById('mePic').value);
+    showContextForm(null);
+}
+
 function IntendeeCell(area) {
     if (!area) return;
     ImgDescCell.call(this, area);
+    this.item = null;
     this.img.className += 'intendeePic';
     this.desc.className += 'intendeeDesc';
+
+    this.emblem = document.createElement('img');
+    this.emblem.style.display = 'none';
+    this.emblem.className += 'intendeeEmblem';
+    area.appendChild(this.emblem);
+
+    var self =this;
+    this.selfUpdate = function() { self.show(self.item); };
+    this.img.onmouseover = function () { self.onClick(); } ;
+    this.desc.onmouseover = function () { self.onClick(); } ;
 }
 
 IntendeeCell.prototype = new ImgDescCell();
@@ -16,14 +53,22 @@ IntendeeCell.prototype.defaultImg = "./sexy_gui/images/guest.gif";
 
 IntendeeCell.prototype.defaultDesc = "???";
 
+IntendeeCell.prototype.defaultEmblem = "./sexy_gui/images/blank.gif";
+
 IntendeeCell.prototype.setCoords = function(inFactor, x) {
     //console.log("inFactor " + inFactor + "; x " + x);
     var gap = this.gap, height = this.height;
     this.img.style.top = ((inFactor - 1.0) * height) + "px";
     this.img.style.left = ((x-1) * gap) + "px";
+    this.emblem.style.top = ((inFactor - 1.0) * height) + "px";
+    this.emblem.style.left = ((x-1) * gap) + "px";
     this.desc.style.top = (inFactor * height) + "px";
     this.desc.style.left = ((x-1) * gap) + "px";
 };
+
+IntendeeCell.prototype.onClick = function() {
+    showContextForm(this);
+}
 
 IntendeeCell.prototype.getOpeningSize = function(area) {
     return area.clientWidth / IntendeeCell.prototype.gap;
@@ -35,6 +80,7 @@ IntendeeCell.prototype.hide = function() {
         this.item.unsubscribe("icon", this.selfUpdate);
         this.item.unsubscribe("nickname", this.selfUpdate);
     }
+    this.emblem.style.display = "none";
     this.img.style.display = "none";
     this.desc.style.display = "none";
     this.item = null;
@@ -46,15 +92,20 @@ IntendeeCell.prototype.show = function(item) {
         if (item.subscribe) {
             item.subscribe("nickname", this.selfUpdate);
             item.subscribe("icon", this.selfUpdate);
+            item.subscribe("emblem", this.selfUpdate);
         }
 
+        this.emblem.style.display = "block";
         this.desc.style.display = "block";
         this.img.style.display = "block";
         this.desc.style.fontStyle = (item === window.me ? "italic" : "normal");
     }
 
     this.img.src = item.icon || this.defaultImg;
+    this.emblem.src = item.emblem || this.defaultEmblem;
     this.desc.innerHTML = item.nickname  || this.defaultDesc;
+    this.desc.style.height = item.mind ? "60px" : "30px";
+    if (item.mind) this.desc.innerHTML += "<br/><quote>" + item.mind + "</quote>";
 };
 
 
@@ -62,8 +113,14 @@ IntendeeCell.prototype.show = function(item) {
 function MessageCell(area) {
     if (!area) return;
     ImgDescCell.call(this, area);
+    this.item = null;
     this.img.className += 'msgPic';
     this.desc.className += 'msgDesc';
+
+    var self =this;
+    this.selfUpdate = function() { self.show(self.item); };
+    this.img.onmouseover = function () { self.onClick(); } ;
+    this.desc.onmouseover = function () { self.onClick(); } ;
 }
 
 MessageCell.prototype = new ImgDescCell();
@@ -119,5 +176,9 @@ MessageCell.prototype.show = function(item) {
     this.desc.innerHTML = item.content || this.defaultDesc;
 
 };
+
+MessageCell.prototype.onClick = function() {
+    showContextForm(this);
+}
 
 MessageCell.prototype.gap = 50;
--- trunk/bus_demo/a2a_chat/sexy_gui/cell.js
+++ trunk/bus_demo/a2a_chat/sexy_gui/cell.js
@@ -1,16 +1,11 @@
 function ImgDescCell(areaElement) {
     if (!areaElement) return;
-    this.item = null;
     this.img = document.createElement('img');
     this.desc = document.createElement('p');
     this.img.style.display = "none";
     this.desc.style.display = "none";
     areaElement.appendChild(this.img);
     areaElement.appendChild(this.desc);
-
-    var self =this;
-    this.selfUpdate = function() { self.show(self.item); };
-    this.img.onclick = function () { self.item.onclick() ; } ;
 }
 
 ImgDescCell.prototype.visible = function(area, x) {
