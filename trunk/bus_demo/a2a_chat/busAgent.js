7b75d0c913fc6a0ab934ee489c0ba7a2449a5f8a
sylvain.garden <sylvain.garden@5f132cf0-ffa0-11dd-a3ef-1f8437fb274a>
2009-07-01 22:41:22 +0000

    autobus now uses dedicated channels for a2a shared objects

--- trunk/bus_demo/a2a_chat/busAgent.js
+++ trunk/bus_demo/a2a_chat/busAgent.js
@@ -1,18 +1,30 @@
 // library "Bus Agent" :
-// A Bus Agent is a Pub/Sub Agent distributed over the web thanks to Handballer HTTP Bus.
+// A Bus Agent is a Pub/Sub Agent distributed over the web thanks to HandBaller HTTP Bus
+// and the "autobus" Javascript framework.
 // - If it is "here", you may directly set its attributs
 // - It it is "there", you may set a value via the bus
 // - It also may be "both" mirrored here and there!
 // =========================================================================
 
-function BusAgent(autobus, name, here) {
-  this.location = (here === undefined ? BusAgent.prototype.there : here);
-
+function BusAgent(autobus, name, location, recipient) {
   this.autobus = autobus;
+  this.location = undefined;
   PubSubAgent.call(this, name, autobus ? autobus.tagsonomy : undefined);
+  if (!autobus) return; // prototype case handling
 
-  if (this.tagsonomy && name && name.length && this.here())
-     this.tagsonomy.pushIn("here", this);
+  this.recipient = recipient ? (recipient.abccli || this.autobus.agora) : this.autobus.agora;
+
+  if ((location === BusAgent.prototype.here || location === BusAgent.prototype.both) && autobus.agora)
+      this.setted("abcli", autobus.hbc.token + autobus.hbc.clientId);
+
+  this.location = (location === undefined ? BusAgent.prototype.there : location);
+
+  if (this.here()) {
+      if (this.tagsonomy && name)
+          this.tagsonomy.pushIn("here", this);
+      
+      this.status();
+  }
 };
 
 BusAgent.prototype = new PubSubAgent();
@@ -22,7 +34,7 @@ BusAgent.prototype.there = 2;
 BusAgent.prototype.both = 3;
 
 BusAgent.prototype.here = function() {
-   return this.location !== BusAgent.prototype.there;
+   return this.location !== undefined && this.location !== BusAgent.prototype.there;
 }
 
 BusAgent.prototype.there = function() {
@@ -42,13 +54,13 @@ BusAgent.prototype.setted = function(variable, newValue) {
     if (variable == "name") {
       if (currentValue) {
         if (!newValue) {
-          this.autobus.hbc.send("freed/" + currentValue);
+          this.autobus.hbc.send(this.recipient + "freed/" + currentValue);
         } else {
-          this.autobus.hbc.send("model/" + currentValue + "/name", jsonize(newValue));
+          this.autobus.hbc.send(this.recipient + "model/" + currentValue + "/name", jsonize(newValue));
         }
       }
     } else {
-      this.autobus.hbc.send("model/" + this.name + "/" + variable, jsonize(newValue));
+      this.autobus.hbc.send(this.recipient + "model/" + this.name + "/" + variable, jsonize(newValue));
     }
   }
 
@@ -74,14 +86,15 @@ BusAgent.prototype.setteds = function(deltaObj) {
   if (!name) name = this.name;
 
   if (this.here() && name)
-    this.autobus.hbc.send("model/" + name, jsonize(deltaObj2));
+    this.autobus.hbc.send(this.recipient + "model/" + name, jsonize(deltaObj2));
 }
 
 BusAgent.prototype.set = function(variable, value) {
    if (this.here()) {
        this.setted(variable, value);
    } else {
-       this.autobus.hbc.send("control/" + this.name + "/set/" + variable, jsonize(value));
+       
+       this.autobus.hbc.send((this.abcli ? this.abcli : this.autobus.agora) + "control/" + this.name + "/set/" + variable, jsonize(value));
    }
 }
 
@@ -89,7 +102,7 @@ BusAgent.prototype.sets = function(deltaObj) {
    if (this.here()) {
        this.setteds(deltaObj);
    } else {
-       this.autobus.hbc.send("control/" + this.name + "/set", jsonize(deltaObj));
+       this.autobus.hbc.send((this.abcli ? this.abcli : this.autobus.agora) + "control/" + this.name + "/set", jsonize(deltaObj));
    }
 }
 
@@ -97,23 +110,23 @@ BusAgent.prototype.call = function(fnName) {
    if (this.here()) {
        this[fnName].apply(this, arguments.splice(1));
    } else {
-       this.autobus.hbc.send("control/" + this.name + "/" + fnName, jsonize(arguments.splice(1)));
+       this.autobus.hbc.send((this.abcli ? this.abcli : this.autobus.agora) + "control/" + this.name + "/" + fnName, jsonize(arguments.splice(1)));
    }
 }
 
-BusAgent.prototype.status = function() {
-
-  if (this.here()) {
+BusAgent.prototype.getStatus = function() {
     var pic = {};  
     for (p in this) {
-      if (typeof this[p] == "function" || !this.cbList[p]) continue;
-      pic[p] = this[p];
+        if (typeof this[p] == "function" || !this.cbList[p]) continue;
+        pic[p] = this[p];
     }
-    this.autobus.hbc.send("model/" + this.name, jsonize(pic));
     return pic;
+}
 
-  } else {
-    this.autobus.hbc.send("control/" + this.name + "/status", jsonize(value));
-    return null;
-  }
+BusAgent.prototype.status = function() {
+    if (this.here()) {
+        var pic = this.getStatus();
+        this.autobus.hbc.send(this.recipient + "model/" + this.name, jsonize(pic));
+    } else
+        this.autobus.hbc.send((this.abcli ? this.abcli : this.autobus.agora) + "control/" + this.name + "/status", jsonize(value));
 }
