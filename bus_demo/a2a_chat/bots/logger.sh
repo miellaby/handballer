7029032854910eaf85b6d56578e062da01bb7769
sylvain.garden <sylvain.garden@5f132cf0-ffa0-11dd-a3ef-1f8437fb274a>
2009-08-22 07:45:51 +0000

    logger bot correction

--- bus_demo/a2a_chat/bots/logger.sh
+++ bus_demo/a2a_chat/bots/logger.sh
@@ -5,17 +5,20 @@ bot=logger
 nickname=Logger
 icon=images/bot.gif
 mind="is logging"
-trap "hbcpost a2ac/freed/$bot" SIGHUP SIGINT SIGTERM
+agora=a2ac
+if [ -n "$1" ] ; then agora=$1 ; fi
+
+trap "hbcpost $agora/freed/$bot" SIGHUP SIGINT SIGTERM
 
 function advertise {
-   hbcpost a2ac/model/$bot "{name:'"$bot$$"',tags:['bot', 'intendee'],nickname:'"$nickname"',icon:'"$icon"',mind:'$mind'}"
+   hbcpost $agora/model/$bot "{name:'"$bot$$"',tags:['bot', 'intendee'],nickname:'"$nickname"',icon:'"$icon"',mind:'$mind'}"
 }
 
 advertise
 
-hbcget 'a2ac/model/m*|a2ac/model/i*|a2ac/status/here' |\
+hbcget "$agora/model/m*|$agora/model/i*|$agora/status/here" |\
      while read l; do
- if [ "$l" == "status/here :" ] ; then
+ if [ "$l" == "$agora/status/here :" ] ; then
      advertise
  fi
  echo $l
