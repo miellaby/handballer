6e11b66b54da80bb595d92ab001a52e761161842
sylvain.garden <sylvain.garden@5f132cf0-ffa0-11dd-a3ef-1f8437fb274a>
2009-10-01 20:50:37 +0000

    beep

--- trunk/bus_demo/a2a_chat/bots/logger.sh
+++ trunk/bus_demo/a2a_chat/bots/logger.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#export HANDBALLER=localhost:81/bus/
-export HANDBALLER=192.168.1.77:81/bus/
+export HANDBALLER=localhost:81/bus/
+#export HANDBALLER=192.168.1.77:81/bus/
 bot=logger
 nickname=Logger
 icon=images/bot.gif
@@ -14,12 +14,17 @@ function advertise {
    hbcpost $agora/model/$bot "{name:'"$bot$$"',tags:['bot', 'intendee'],nickname:'"$nickname"',icon:'"$icon"',mind:'$mind'}"
 }
 
+function beep {
+   if [ -f /usr/bin/Set_Led ] ; then /usr/bin/Set_Led beep1 ; fi
+}
+
 advertise
 
 hbcget "$agora/model/m*|$agora/model/i*|$agora/status/here" |\
      while read l; do
  if [ "$l" == "$agora/status/here :" ] ; then
      advertise
+     beep
  fi
  echo $l
  echo $l >> /tmp/bot_logger.txt
