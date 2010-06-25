93e72b6113138124561557b0cbeee9b0981f5d92
sylvain.garden <sylvain.garden@5f132cf0-ffa0-11dd-a3ef-1f8437fb274a>
2010-06-25 14:36:09 +0000

    admin scripts

--- extras.handballer/unslug/unslug-install.sh
+++ extras.handballer/unslug/unslug-install.sh
@@ -1,8 +1,14 @@
+# use this script to install/update handballer installation in an unslug box.
+# or an optware-enabled linux device.
 cp ./handballer /opt/bin
 cp libhbc/libhbc.so /opt/lib
 cp libhbc/hbcget /opt/bin
 cp libhbc/hbcpost /opt/bin
+mkdir -p /opt/local/bin
+cp extras/unslug/S45handballer.sh /opt/etc/init.d
+cp extras/unslug/handballer_bots /opt/local/bin
 strip /opt/bin/handballer /opt/bin/hbc* /opt/lib/libhbc.so
-cp -r ./bus_demo /opt/var/www
-chmod -R a+rX /opt/var/www/*
+#cp -r ./bus_demo/* /opt/var/www
+rsync --exclude=.svn -au ./bus_demo/. /opt/var/www/.
+chmod -R a+rX /opt/var/www
 
